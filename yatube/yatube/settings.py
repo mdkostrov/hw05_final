bd7ee87b75a4b126465a40e8341fb4de2311bf6e
Михаил Костров <mdkostrov@gmail.com>
2023-03-10 15:18:44 +0300

    debug-false и allowed_hosts

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,13 +23,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u5^ybn4wl&1*w4*5a1e!v8qs3*0_n0lcdmdi4331(8rbon36$z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '[::1]',
     'testserver',
+    '51.250.83.101',
 ]
 
 
